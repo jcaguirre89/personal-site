05f4173825a885376e636a31f9ae752864ff0260
Cristobal <cristobal.aguirre89@gmail.com>
2019-08-22 11:19:00 -0700

    custom fetch hook for twitter lambda api

--- src/components/getTwitterData.js
+++ src/components/getTwitterData.js
@@ -1,37 +1,88 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
 
-function GetTwitterData({terms, lang, apiKey}) {
-  const url =
-    'https://zs63emuqmk.execute-api.us-east-1.amazonaws.com/default/scrape_twitter_api';
-
-  const body = JSON.stringify({
-    terms,
-    lang,
+const useTwitterApi = () => {
+  const [data, setData] = useState([]);
+  const [params, setParams] = useState({
+    url:
+      'https://5zoc7b1wnf.execute-api.us-east-1.amazonaws.com/default/scrape_twitter_api',
+    terms: 'Santiago',
+    lang: 'es',
   });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-  console.log(body);
-
-  const getData = async (terms, lang) => {
-    const data = fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'x-api-key': apiKey,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        terms,
-        lang,
-      }),
-    })
-      .then(response => response.json())
-      .catch(error => console.error(error));
-    return data;
+  const requestOptions = {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: JSON.stringify({
+      terms: params.terms,
+      lang: params.lang,
+    }),
   };
 
-  console.log(getData(terms, lang));
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
 
-  return <pre>{JSON.stringify(getData(terms, lang))}</pre>;
+      try {
+        const result = await axios(params.url, requestOptions);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [params]);
+
+  return [{data, isLoading, isError}, setParams];
+};
+
+function GetTwitterData() {
+  const [terms, setTerms] = useState('Chile')
+  const lang = 'es'
+  const url =
+    'https://5zoc7b1wnf.execute-api.us-east-1.amazonaws.com/default/scrape_twitter_api';
+
+  const requestOptions = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      terms,
+      lang,
+    }),
+  };
+  
+  const [{ data, isLoading, isError }, doFetch] = useTwitterApi();
+
+  console.log(data);
+  return (
+    <div>
+      <form onSubmit={event => {
+        doFetch({url, terms, lang})
+
+        event.preventDefault();
+      }}>
+        <input
+          type="text"
+          value={terms}
+          onChange={event => setTerms(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+
+    </div>
+  );
 }
 
 export default GetTwitterData;
