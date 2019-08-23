d0ccdf404a9af89fdebf8c5d1dbc8358ac46930c
Cristobal <cristobal.aguirre89@gmail.com>
2019-08-23 10:36:12 -0700

    started with disqus:

--- src/@narative/gatsby-theme-novela/templates/article.template.tsx
+++ src/@narative/gatsby-theme-novela/templates/article.template.tsx
@@ -1,6 +1,32 @@
-import Article from "@narative/gatsby-theme-novela/src/templates/article.template";
+import React from 'react'
+import Article from '@narative/gatsby-theme-novela/src/templates/article.template';
+import {Disqus} from 'gatsby-plugin-disqus';
+import {useStaticQuery, graphql} from 'gatsby';
 
-import "katex/dist/katex.min.css";
-import "../../../styles/katex-extra.css";
+import 'katex/dist/katex.min.css';
+import '../../../styles/katex-extra.css';
 
-export default props => <Article {...props} />;
+export default props => {
+  const data = useStaticQuery(graphql`
+    query ArticleQuery {
+      site {
+        siteMetadata {
+          siteUrl
+        }
+      }
+    }
+  `);
+  const {pageContext, location} = props;
+  const {article} = pageContext;
+  const disqusConfig = {
+    url: data.site.siteMetadata.siteUrl,
+    identifier: article.id,
+    title: article.title,
+  };
+  return (
+    <React.Fragment>
+      <Article {...props} />
+      <Disqus config={disqusConfig} />
+    </React.Fragment>
+  );
+};
