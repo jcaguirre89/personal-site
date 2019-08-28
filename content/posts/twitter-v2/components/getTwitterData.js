3bd14892a6095e4c275393a8bed5be1c61f03d5d
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-08-28 00:31:58 -0700

    Added apollo client to query twitter words

--- content/posts/twitter-v2/components/getTwitterData.js
+++ content/posts/twitter-v2/components/getTwitterData.js
@@ -1,90 +1,52 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
+import {useQuery } from '@apollo/react-hooks'
+import gql from "graphql-tag";
 import BubbleChart from './bubbleChart';
 import HashLoader from 'react-spinners/HashLoader';
 import {useColorMode} from 'theme-ui';
 import { Field } from "@leveluptuts/fresh";
 import StyledForm from '../../../../src/components/styles/form'
 
-const useTwitterApi = () => {
-  const [data, setData] = useState([]);
-  const [params, setParams] = useState({
-    url:
-      'https://5zoc7b1wnf.execute-api.us-east-1.amazonaws.com/default/scrape_twitter_api',
-    terms: 'Maradona',
-    lang: 'es',
-  });
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-
-  const requestOptions = {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: JSON.stringify({
-      terms: params.terms,
-      lang: params.lang,
-      max_size: 500,
-    }),
-  };
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsError(false);
-      setIsLoading(true);
-
-      try {
-        const result = await axios(params.url, requestOptions);
-
-        setData(result.data);
-      } catch (error) {
-        setIsError(true);
-      }
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, [params]);
-
-  return [{data, isLoading, isError}, setParams];
-};
+const GET_WORDS_QUERY = gql`
+query getWords($terms: String!, $language: String) {
+  words(terms: $terms, language: $language)
+}
+`;
 
 function GetTwitterData() {
-  const url =
-    'https://5zoc7b1wnf.execute-api.us-east-1.amazonaws.com/default/scrape_twitter_api';
-
-  const [{data, isLoading, isError}, doFetch] = useTwitterApi();
-
+  const { loading, error, data: {words}, refetch } = useQuery(GET_WORDS_QUERY, {
+    variables: { terms: 'Maradona', language: 'es' }
+  });
   const [colorMode] = useColorMode();
   const fill = colorMode === 'dark' ? '#fff' : '#000';
 
-  const wordCounts = data.reduce((obj, item) => {
-    if (!obj[item]) {
-      obj[item] = 0;
+
+    const wordCounts = words && words.reduce((obj, item) => {
+      if (!obj[item]) {
+        obj[item] = 0;
     }
     obj[item]++;
     return obj;
-  }, {});
+    }, {});
 
-  const wordCountArray = Object.entries(wordCounts)
+    const wordCountArray = words && Object.entries(wordCounts)
     .filter(([k, v]) => v > 2)
     .map(([k, v]) => {
       return {name: k, value: v};
     });
 
-  const bubbleChartRoot = {
-    name: 'root',
-    children: wordCountArray,
-  };
+    const bubbleChartRoot = {
+      name: 'root',
+      children: wordCountArray || [],
+    };
 
   return (
     <div style={{display: 'flex', flexDirection: 'column'}}>
       <StyledForm
-        onSubmit={({data}) => {
-          const lang = data.language || 'es';
-          doFetch({url, terms: data.terms, lang})}
-        }
+        onSubmit={({data: formData}) => {
+          const lang = formData.language || 'es';
+          refetch({terms: formData.terms, language: lang})
+        }}
         cancelButton={false}
         >
           <Field placeholder='Maradona' defaultValue='Maradona'>Terms</Field>
@@ -99,8 +61,8 @@ function GetTwitterData() {
           alignItems: `center`,
           justifyContent: `center`,
         }}>
-        <HashLoader size="80" color={fill} loading={isLoading} />
-        {!isLoading && wordCountArray && wordCountArray.length > 0 && (
+        <HashLoader size="80" color={fill} loading={loading} />
+        {!loading && bubbleChartRoot.children.length && (
           <BubbleChart colorMode={colorMode} root={bubbleChartRoot} />
         )}
       </div>
