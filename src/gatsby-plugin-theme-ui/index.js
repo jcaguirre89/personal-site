381e3de2dda4e85930a38b6d18b14f032cb9d627
Cristobal Aguirre <cristobal.aguirre89@gmail.com>
2019-09-18 13:39:55 -0700

    switched to gatsby theme ui proper, new novela theme and grotesk fonts

---
+++ src/gatsby-plugin-theme-ui/index.js
@@ -0,0 +1,37 @@
+import novelaTheme from "@narative/gatsby-theme-novela/src/gatsby-plugin-theme-ui";
+
+const offBlack = "#242629";
+const offWhite = "#eeeeef";
+
+const fonts = {
+  serif: "'Merriweather', Georgia, Serif",
+  sansSerif:
+    "'HK Grotesk', 'SF Pro Display', '-apple-system', 'BlinkMacSystemFont', 'San Francisco', 'Helvetica Neue', 'Helvetica', 'Ubuntu', 'Roboto', 'Noto', 'Segoe UI', 'Arial', sans-serif",
+  monospace: `"Operator Mono", Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`
+};
+
+export default {
+  ...novelaTheme,
+  initialColorMode: `light`,
+  fonts,
+  colors: {
+    ...novelaTheme.colors,
+    primary: offBlack,
+    secondary: offBlack,
+    grey: "#73737D",
+    background: "#fff",
+    accent: "#6166DC",
+    articleText: offBlack,
+    modes: {
+      dark: {
+        grey: "#73737D",
+        primary: offWhite,
+        secondary: offWhite,
+        accent: "hsla(9, 80%, 70%, 1)",
+        background: offBlack,
+        gradient: `linear-gradient(180deg, ${offBlack} 0%, rgba(66, 81, 98, 0.36) 100%)`,
+        articleText: offWhite
+      }
+    }
+  }
+};
