ec7a5ce7e0e8449b22ce5c689223495f641672ab
Cristobal Aguirre <cristobal@pop-os.localdomain>
2020-08-22 10:33:35 -0400

    removed contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,13 +76,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-contentful",
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Cristobal Aguirre`,
